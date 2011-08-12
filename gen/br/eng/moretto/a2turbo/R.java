255a8654fc55e2d6a95f6da99ce7dd65382c2866
Emerson Moretto <emerson@moretto.eng.br>
2011-08-12 16:05:02 -0300

    lixo

--- gen/br/eng/moretto/a2turbo/R.java
+++ gen/br/eng/moretto/a2turbo/R.java
@@ -11,19 +11,20 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int logo=0x7f020001;
-        public static final int logo_src=0x7f020002;
-        public static final int plastic=0x7f020003;
-        public static final int progress_vertical=0x7f020004;
-        public static final int seek_thumb=0x7f020005;
-        public static final int seek_thumb_normal=0x7f020006;
-        public static final int seek_thumb_normal_wide=0x7f020007;
-        public static final int seek_thumb_pressed=0x7f020008;
-        public static final int seek_thumb_pressed_wide=0x7f020009;
-        public static final int seek_thumb_selected=0x7f02000a;
-        public static final int seek_thumb_selected_wide=0x7f02000b;
-        public static final int seek_thumb_wide=0x7f02000c;
+        public static final int custom_seekbar=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int logo=0x7f020002;
+        public static final int logo_src=0x7f020003;
+        public static final int plastic=0x7f020004;
+        public static final int progress_vertical=0x7f020005;
+        public static final int seek_thumb=0x7f020006;
+        public static final int seek_thumb_normal=0x7f020007;
+        public static final int seek_thumb_normal_wide=0x7f020008;
+        public static final int seek_thumb_pressed=0x7f020009;
+        public static final int seek_thumb_pressed_wide=0x7f02000a;
+        public static final int seek_thumb_selected=0x7f02000b;
+        public static final int seek_thumb_selected_wide=0x7f02000c;
+        public static final int seek_thumb_wide=0x7f02000d;
     }
     public static final class id {
         public static final int LinearLayout01=0x7f060007;
@@ -46,13 +47,13 @@ public final class R {
         public static final int fuelmap=0x7f06001a;
         public static final int new_devices=0x7f060005;
         public static final int paired_devices=0x7f060003;
-        public static final int seekBar1=0x7f060018;
-        public static final int text_view=0x7f060017;
+        public static final int seekBar1=0x7f060016;
+        public static final int text_view=0x7f060018;
         public static final int title_left_text=0x7f060000;
         public static final int title_new_devices=0x7f060004;
         public static final int title_paired_devices=0x7f060002;
         public static final int title_right_text=0x7f060001;
-        public static final int turbogauge=0x7f060016;
+        public static final int turbogauge=0x7f060017;
     }
     public static final class layout {
         public static final int custom_title=0x7f030000;
